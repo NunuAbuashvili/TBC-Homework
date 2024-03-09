1c9a4786fbb3928a7ec7aa11780ee4de21097df1
Nunu Abuashvili <abuashvilinunu@gmail.com>
2024-03-09 22:03:23 +0400

    Restored deleted folder

---
+++ Homework_1/Task_2.py
@@ -0,0 +1,17 @@
+# Ask the user for their name
+user_name = input("Enter your name: ")
+
+# Ask the user for their birth year
+user_birth_year = int(input("Enter your birth year: "))
+
+# Create a variable to store the information of the current year
+current_year = 2024
+
+# Calculate the user's age
+user_age = current_year - user_birth_year
+
+# Print a greeting message
+if user_birth_year > current_year:
+    print("Please, enter your real birth year.")
+else:
+    print("Hello,", user_name, ", you are", user_age, "years old.")
