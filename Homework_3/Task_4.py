085ea1bdaa8dad99d42e7d5309740306ca246c82
Nunu Abuashvili <abuashvilinunu@gmail.com>
2024-03-09 19:06:39 +0400

    Fixed some mistakes

--- Homework_3/Task_4.py
+++ Homework_3/Task_4.py
@@ -9,33 +9,35 @@ print("Enter the date (YY/MM/DD) you bought your bitcoin, also the dollar amount
 btc_purchase_year = int(input("Enter the year you bought the bitcoin: "))
 btc_purchase_month = int(input("Enter the month you bought the bitcoin: "))
 btc_purchase_day = int(input("Enter the day you bought the bitcoin: "))
-btc_purchase_price = float(input("Enter the amount in USD that you've paid for the bitcoin: "))
-
-# Format the puchase date
-purchase_date = datetime.date(btc_purchase_year, btc_purchase_month, btc_purchase_day)
+btc_amount_paid = float(input("Enter the amount in USD that you've paid for the bitcoin: "))
 
 # Create a BtcConverter object
 btc = BtcConverter()
 
+# Format the puchase date
+purchase_date = datetime.date(btc_purchase_year, btc_purchase_month, btc_purchase_day)
+
+# Calculate the amount of bitcoin purchased
+btc_purchase_price = btc.get_previous_price('USD', purchase_date)
+btc_amount = btc_amount_paid / btc_purchase_price
+
+# Get the current year
+today = datetime.date.today()
+current_year = today.year
+
 # Get the price of bitcoin today (in USD)
 btc_price_today = btc.get_latest_price('USD')
 
-# Get today's date
-today = datetime.date.today()
-
-# Get the current year
-current_year = today.year
-
 # Calculate profit or loss
-profit_loss = btc_price_today - btc_purchase_price
+profit_loss = (btc_amount * btc_price_today) - btc_amount_paid
 
 # Print the result (profit, loss or the same)
 if btc_purchase_year > current_year:
     print("Please, enter the true year you bought the bitcoin, try again!")
 else:
     if profit_loss > 0:
-        print("Congratulations! As of", today, ", you made a profit of", profit_loss,  "USD")
+        print(f"Congratulations! You made a profit of {profit_loss} USD")
     elif profit_loss < 0:
-        print("Sorry! As of", today, ", you suffered a loss of", abs(profit_loss))
+        print(f"Sorry! You suffered a loss of {abs(profit_loss)} USD")
     else:
-        print("As of", today, ", you neither made a profit nor suffered a loss.")
+        print("You neither made a profit nor suffered a loss.")
