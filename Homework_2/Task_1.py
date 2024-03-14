1984dca9c0c7512c632245bd82934627fc14dcc4
Nunu Abuashvili <abuashvilinunu@gmail.com>
2024-03-14 18:56:05 +0400

    Updated the variable name according to snake_case

--- Homework_2/Task_1.py
+++ Homework_2/Task_1.py
@@ -1,11 +1,11 @@
 # დაწერეთ პროგრამა რომელიც მიიღებს true ან false. თუ პროგრამამ მიიღო true, ეკრანზე დაბეჭდეთ “whoala”.
 
 # Let the user enter True or False
-isTrue = input("Enter True or False: ")
+is_true = input("Enter True or False: ")
 
-if isTrue == "True" or isTrue == "true":
+if is_true == "True" or is_true == "true":
     print("Whoala!")
-elif isTrue == "False" or isTrue == "false":
+elif is_true == "False" or is_true == "false":
     print("You entered 'False'!")
 else:
     print("You didn't enter 'True' or 'False'!")
