ed6842a37588863983845ef7cc02f541c9b13605
Nunu Abuashvili <abuashvilinunu@gmail.com>
2024-03-29 22:20:52 +0400

    Fixed a mistake

--- Homework_8/Task_2.py
+++ Homework_8/Task_2.py
@@ -5,15 +5,14 @@ CONSONANTS = 'BCDFGHJKLMNPQRSTVWXYZ'
 
 # Ask user to enter a word and convert it to uppercase to match the consonants
 user_input = input("Please, enter a word: ")
-user_input = user_input.upper()
 
 # Initialize en empty string to store the output word containing only consonants
 output_word = ''
 
 # Iterate over each character in the user input and append consonants to the output_word
 for c in user_input:
-    if c in CONSONANTS:
+    if c.upper() in CONSONANTS:
         output_word += c
 
 # Print the output after converting it to lowercase
-print(output_word.lower())
+print(output_word)
