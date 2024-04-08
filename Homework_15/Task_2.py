d2477a893b11b7c2b1e147a640ee140cac80170a
Nunu Abuashvili <abuashvilinunu@gmail.com>
2024-04-08 23:20:14 +0400

    Fixed a mistake

--- Homework_15/Task_2.py
+++ Homework_15/Task_2.py
@@ -4,16 +4,10 @@ def is_prime(number: int):
     # This function checks if a given integer is prime or not
     
     # Negative numbers, 0 and 1 are not prime numbers
-    if number < 2:
+    if number <= 1:
         return 'IS NOT A PRIME NUMBER'
     
-    # 2 is the only even prime number
-    if number == 2:
-        return 'IS A PRIME NUMBER'
-    
-    # Iterate from 3 to 'n // 2', step = 2
-    upper_range = number // 2
-    for i in range(3, upper_range, 2):
+    for i in range(2, number):
         if number % i == 0:
             return 'IS NOT A PRIME NUMBER'
         
@@ -26,6 +20,8 @@ def main():
     test_number_three = -5
     test_number_four = 1
     test_number_five = 2
+    test_number_six = 8
+    test_number_seven = 3
     
     # Print results
     print(test_number_one, is_prime(test_number_one)) # NO
@@ -33,6 +29,8 @@ def main():
     print(test_number_three, is_prime(test_number_three)) # NO
     print(test_number_four, is_prime(test_number_four)) # NO
     print(test_number_five, is_prime(test_number_five)) # YES
+    print(test_number_six, is_prime(test_number_six)) # YES
+    print(test_number_seven, is_prime(test_number_seven)) # YES
 
 if __name__ == "__main__":
     main()
